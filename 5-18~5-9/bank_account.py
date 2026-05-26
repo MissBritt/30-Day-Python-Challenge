fff98954f280f957389dd702c87acda5de13e7a3
Brittany <missbritt212@gmail.com>
2026-05-25 20:49:03 -0400

    Developing a program that simulates function of a bank account

--- 5-18~5-9/bank_account.py
+++ 5-18~5-9/bank_account.py
@@ -1,22 +1,53 @@
-# Bank account program
+# Bank account program (unfinished)
 
 import random
 import time
 passcode = 4321
+balance = 100
+
 
 # BANK INTERFACE
-
 def bank_interface():
-   for i in range(10):
-    print(".")
+    welcome_msg = f"\t-------------Welcome {username}-------------\t"
+    main_ui = f"\t| Current Balance: {balance} |\t\n\n\t[1]DEPOSIT\t[2]WITHDRAW"
+    global balance
+  
+    for i in range(10):
+        print(".")
    
-   welcome_msg = f"\t-------------Welcome {username}-------------\t"
-   ui_list = ["d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "d"]
-   print(ui_list)
-   print(welcome_msg.upper())
+    print(welcome_msg.upper())
+    print(main_ui)
+    ui_nav = int(input("\nSelect Option>> "))
+    while True:
+        if ui_nav == 1:
+            print("Deposit menu")
+            deposit_input = int(input("How much would you like to deposit? >> $"))
+            if deposit_input == deposit_input:
+                balance += deposit_input
+                time.sleeo(1)
+                print("Balance added successfully!")
+                bank_interface()
+                break
+            break
+        elif ui_nav == 2:
+            print("Withdrawl")
+            break
+        else:
+            print("Not a valid choice, please try again")
+    
+#THE ARE YOU SURE? FUNCTION
+"""def ask_again():
+    ask_input = int(input("Are you sure you want to select this option?\n[1]Yes\t[2]Cancel\nSelect Option>> "))
+    while True:
+        if ask_input == 1:
+            break
+        elif ask_input == 2:
+            bank_interface()
+        else:
+            print("Not a valid choice, please try again")"""
+
 
 #LAUNCH ANIMATION/INITIATION
-
 for i in range(4):
     pass
     time.sleep(1)
---
+++ readme.md
@@ -0,0 +1,16 @@
+PREFACE
+
+This whole challenge is to revitalize/re learn my foundational knowledge on
+programming, noteably for python in particular...
+
+This file will be updated daily noting the progress on projects and things I learned, while the folders will contain project files I have had been working on or projects I am currently working on. This will also include tweaks and revisions that I may be doing to old projects that will also be documented here.
+
+I wrote this README file a couple days into the challenge so you way see some changes I made to this document now that occured in the past.
+
+I officially initiated this challenge on May 17 2026 and will end ending this repo on June 20th. (I know that is a little more than 30 days, but I want to add extra days for the sake of going back to make new revisions or features of old projects that I may dedicate a little more time to).
+
+Projects may be posted once to a few days, but progress will be documented daily, along with some commentary on what I have learned or some additional thoughts I have had during this venture.
+
+Think of this as a little blog :D
+
+[DOCUMENT WIP]
